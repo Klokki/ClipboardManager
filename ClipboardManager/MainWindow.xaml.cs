db6235b10235f10f3e5012e3a0ba7d63b4ad3403
Sami Kivinen <21125844+Klokki@users.noreply.github.com>
2018-12-27 23:48:42 +0200

    minimize to tray using winforms

--- ClipboardManager/MainWindow.xaml.cs
+++ ClipboardManager/MainWindow.xaml.cs
@@ -19,6 +19,29 @@ namespace ClipboardManager
 
             var clipHandler = new ClipboardHandler(this);
             clipHandler.ClipboardChanged += ClipboardUpdate;
+
+            // minimizing the window using winforms NotifyIcon
+            // maybe switch to WPF NotifyIcon later
+            System.Windows.Forms.NotifyIcon ni = new System.Windows.Forms.NotifyIcon();
+            ni.Icon = new System.Drawing.Icon(@"../../Clip.ico");
+            ni.Visible = true;
+            ni.DoubleClick +=
+                delegate (object sender, EventArgs args)
+                {
+                    this.Show();
+                    this.WindowState = WindowState.Normal;
+                };
+        }
+
+        /// <summary>
+        /// minimizing WPF form using winforms
+        /// </summary>
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState == System.Windows.WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
         }
 
         private void ClipboardUpdate(object sender, EventArgs e)
